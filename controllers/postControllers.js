d9579261dbaf071dc7b4a469382c73d09136e060
kuruvapavani <pavanikuruva2109@gmail.com>
2024-05-09 16:13:42 +0530

    fix import error

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -8,10 +8,14 @@ import User from "../models/userModel.js";
 import HttpError from "../models/errorModel.js";
 import { dirname, join } from 'path';
 import {google} from 'googleapis';
-import { oauth2Client } from './path/to/oauth2ClientModule';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 // create new post
 
