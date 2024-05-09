901827f708d0c8675a8cdc19c8a8b8d52ba4ea2f
kuruvapavani <pavanikuruva2109@gmail.com>
2024-05-09 16:05:07 +0530

    fix error

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -8,6 +8,7 @@ import User from "../models/userModel.js";
 import HttpError from "../models/errorModel.js";
 import { dirname, join } from 'path';
 import {google} from 'googleapis'
+const { oauth2Client } = require('./path/to/oauth2ClientModule');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
